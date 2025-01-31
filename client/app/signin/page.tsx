533cbfebbc532d6e65f00513abc1d56f37f64c5c
Deezi <deezitheviper@gmail.com>
2025-01-31 05:24:17 +0100

    client

---
+++ client/app/signin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">Signin Page.</h1>
+  )
+}
+
+export default page
---
+++ client/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">Signup Page.</h1>
+  )
+}
+
+export default page
