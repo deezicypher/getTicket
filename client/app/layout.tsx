4176b33fd56c728349d9b291b88a8427425503c2
Deezi <deezitheviper@gmail.com>
2025-02-03 05:08:51 +0100

    sign page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,21 @@
+"use client"
 import React from 'react'
 import './globals.css'
+import {Toaster} from 'react-hot-toast'
+import {QueryClientProvider, QueryClient} from 'react-query'
+import { AuthContextProvider } from './context/authContext'
+
+
+const queryClient = new QueryClient(
+  {
+     defaultOptions:{
+        queries:{
+           refetchOnMount:true,
+           refetchOnWindowFocus: false,
+        }
+     }
+  }
+)
 
 export default function RootLayout({
   children,
@@ -8,7 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <AuthContextProvider>   
+           <body>{children}</body>
+           </AuthContextProvider>
+
+      </QueryClientProvider>
     </html>
   )
 }
