bf02f9c369ba2a5520721b9e56df28345fcbbb00
Deezi <deezitheviper@gmail.com>
2025-03-19 19:27:11 +0100

    base files

---
+++ commonx/src/server.ts
@@ -0,0 +1,6 @@
+export * from './middlewares/verify';
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
